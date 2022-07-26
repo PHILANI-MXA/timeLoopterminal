d676c4a2e6a3af71f4e7957140866b2457d1f3dc
PHILANI-MXA <3911755@myuwc.ac.za>
2022-07-26 11:51:47 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Day 2 nodejs example")
+/*console.log("Day 2 nodejs example")
 
 setTimeout(function() {
     console.log('3 seconds have passed');
@@ -12,5 +12,41 @@ var timer = setInterval(function () {
     if (time > 5) {
         clearInterval(timer);
     }
-}, 2000);
+}, 2000);*/
+
+/*const EventEmitter = require('events');
+const { EventEmitter } = require('stream');
+const EventEmitter = new EventEmitter();
+
+EventEmitter.on('tutorial', (num1,num2) => {
+    console.log(num1 + num2);
+});
+
+EventEmitter.emit('tutorial',1,2);*/
+
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
 
